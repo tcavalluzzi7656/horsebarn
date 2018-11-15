74ce0bb85dfc3883ea3ec2448f408baf381656ec
BT_1N3_11@BKTECH.LOCAL <BT_1N3_11@BKTECH.LOCAL>
2018-11-15 14:48:17 -0500

    HORSE BE LIKE. NEIGH.

---
+++ src/HorseBarn.java
@@ -0,0 +1,13 @@
+public class HorseBarn
+{
+    /
+    private horse[] spaces;
+
+     */
+    public int findHorseSpace(String name)
+    { /* to be implemented in part (a) */ }
+
+    public void consolidate()
+    { /* to be implemented in part (b) */ }
+// There may be instance variables, constructors, and methods that are not shown.
+}
---
+++ src/horse.java
@@ -0,0 +1,6 @@
+public interface horse {
+    /** @return the horse's name */
+    String getName();
+    /** @return the horse's weight */
+    int getWeight();
+}
